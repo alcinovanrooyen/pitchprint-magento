d4e0ea0428768c22c142d19c261ee604e03c612a
root <root@ip-172-26-13-81.eu-west-1.compute.internal>
2018-06-19 17:40:06 +0200

    return PitchPrint project ID if appended to order item

--- PitchPrint/Block/Adminhtml/Order/View/Items/Renderer/PitchPrintRenderer.php
+++ PitchPrint/Block/Adminhtml/Order/View/Items/Renderer/PitchPrintRenderer.php
@@ -5,8 +5,23 @@ namespace PitchPrintInc\PitchPrint\Block\Adminhtml\Order\View\Items\Renderer;
 class PitchPrintRenderer extends \Magento\Sales\Block\Adminhtml\Order\View\Items\Renderer\DefaultRenderer
 {
     
-    public function fetchdPpId()
+    public function fetchPpId()
     {
-        return '123cinowas here';
+		$options 	= $this->getItem()['product_options']['info_buyRequest'];
+		$ppId 		= null;
+		
+        if ( isset($options['_pitchprint']) )
+        {
+            return $options['_pitchprint'];
+        }
+        
+		return $ppId;
+    }
+    
+    private function consoleLog($item)
+    {
+        $item = json_encode($item);
+        
+        echo "<script>console.log($item);</script>";
     }
 }
