bad69e94474753afda13b4abe6b7773586faea4b
root <root@ip-172-26-13-81.eu-west-1.compute.internal>
2018-06-19 16:14:08 +0200

    New class that get's used on order item in backend

---
+++ PitchPrint/Block/Adminhtml/Order/View/Items/Renderer/PitchPrintRenderer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PitchPrintInc\PitchPrint\Block\Adminhtml\Order\View\Items\Renderer;
+
+class PitchPrintRenderer extends \Magento\Sales\Block\Adminhtml\Order\View\Items\Renderer\DefaultRenderer
+{
+    
+    public function fetchdPpId()
+    {
+        return '123cinowas here';
+    }
+}
