ecf4758302e9f89ec2b891f456c8b8a76e07c6c9
root <root@ip-172-26-13-81.eu-west-1.compute.internal>
2019-05-02 12:36:49 +0200

    deleted inner copy of module

--- PitchPrint/Config/Constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace PitchPrintInc\PitchPrint\Config;
-
-class Constants {
-    const TABLE_CONFIG 			= 'pitch_print_config';
-    const TABLE_PRODUCT_DESIGN 	= 'pitch_print_product_design';
-}
--- PitchPrint/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
- 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'PitchPrintInc_PitchPrint',
-    __DIR__
-);
