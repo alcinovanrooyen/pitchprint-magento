b36a10085aeb68d2312162ed5637a95092fd5ce9
root <root@ip-172-26-13-81.eu-west-1.compute.internal>
2019-05-02 16:32:35 +0200

    pull data into order from quote data table

--- Block/Adminhtml/Order/View/Items/Renderer/PitchPrintRenderer.php
+++ Block/Adminhtml/Order/View/Items/Renderer/PitchPrintRenderer.php
@@ -5,19 +5,23 @@ namespace PitchPrintInc\PitchPrint\Block\Adminhtml\Order\View\Items\Renderer;
 class PitchPrintRenderer extends \Magento\Sales\Block\Adminhtml\Order\View\Items\Renderer\DefaultRenderer
 {
     
-    public function fetchPpId()
+    public function fetchPpData()
     {
-		$options 	= $this->getItem()['product_options']['info_buyRequest'];
-		$ppId 		= null;
-		
-        if ( isset($options['_pitchprint']) )
-        {
-            return $options['_pitchprint'];
-        }
-        
-		return $ppId;
+		$data = $this->getProjectData($this->getItem()->getQuoteItemId());
+		if ($data) return $data[0]['project_data'];
+		return 0;
     }
     
+    private function getProjectData($quoteId) {
+	    $objectManager  = \Magento\Framework\App\ObjectManager::getInstance();
+        $resource       = $objectManager->get('Magento\Framework\App\ResourceConnection');
+        $db             = $resource->getConnection(); 
+        $tableName      = $resource->getTableName(\PitchPrintInc\PitchPrint\Config\Constants::TABLE_QUOTE_ITEM); //gives table name with prefix
+		$sql            = "SELECT `project_data` FROM $tableName WHERE `quote_id` = $quoteId";
+        $data           = $db->fetchAll( $sql );
+        return $data;
+	}
+	
     private function consoleLog($item)
     {
         $item = json_encode($item);
